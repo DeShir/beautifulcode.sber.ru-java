d86fec97f3f3c00932e483dcc79152c23004fe20
Denis Shirokov <de.sh.vit@gmail.com>
2023-09-12 11:20:53 +0300

    парсер, распознающий текст со скобками

---
+++ src/main/java/ru/sber/beautifulcode/syntaxchecker/common/StringExtension.java
@@ -0,0 +1,21 @@
+package ru.sber.beautifulcode.syntaxchecker.common;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *  Расширение для строк
+ */
+public class StringExtension {
+    /**
+     * Вернет представление строки в виде списка символов
+     * @param str исходная строка
+     * @return список символов
+     */
+    @NotNull
+    public static List<Character> toCharList(@NotNull String str) {
+        return str.chars().mapToObj(it -> (char) it).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/ru/sber/beautifulcode/syntaxchecker/parsers/PresetParsers.java
@@ -0,0 +1,52 @@
+package ru.sber.beautifulcode.syntaxchecker.parsers;
+
+import lombok.experimental.UtilityClass;
+import ru.sber.beautifulcode.syntaxchecker.parser.Parser;
+import ru.sber.beautifulcode.syntaxchecker.parser.Parsers;
+import ru.sber.beautifulcode.syntaxchecker.parser.SymbolKind;
+
+import static ru.sber.beautifulcode.syntaxchecker.parser.Parsers.*;
+import static ru.sber.beautifulcode.syntaxchecker.parser.Parsers.is;
+
+/**
+ * Утилитный класс для стандратных парсеров
+ */
+@UtilityClass
+public class PresetParsers {
+
+    /**
+     * Вернер парсер, распознающий текст с грамматикой:
+     * T -> W.T
+     * T -> W
+     * W -> (.T.)
+     * W -> s
+     * где s - любой символ, кроме '(', ')'
+     *     s, '(', ')' - терминальные символы
+     *
+     * @return парсер, распознающий текст со скобками
+     */
+    public Parser<Character, SymbolKind> parensWithText() {
+
+        var s = satisfy(isnt('(', ')'));
+        var o = satisfy(is('('));
+        var c = satisfy(is(')'));
+
+        var w_ = Parsers.<Character, SymbolKind>lazy();
+        var t_ = Parsers.<Character, SymbolKind>lazy();
+
+        var w = or(
+                seq(o, t_, c),
+                s
+        );
+
+        var t = or(
+                seqr(w, t_),
+                w
+        );
+
+        w_.set(w);
+        t_.set(t);
+
+        return just(t);
+    }
+}
